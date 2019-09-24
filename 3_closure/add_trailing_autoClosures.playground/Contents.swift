31e6e6ea538b1d5f9b3feee5a0e07c51e8f705ec
MacMini2012 <macmini2012@MacMini2012s-Mac-mini.local>
2019-09-24 13:35:08 +0700

    closures

---
+++ 3_closure/add_trailing_autoClosures.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+struct Cuboid {
+    var width = 0.0, height = 0.0, depth = 0.0
+    var volume: Double {
+        return width * height * depth
+    }
+}
+var fourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+print("the volume of fourByFiveByTwo is \(fourByFiveByTwo.volume)")        // 40.0
+fourByFiveByTwo.width = 20.0
+print("the volume of fourByFiveByTwo is \(fourByFiveByTwo.volume)")        // 200.0
+
+
